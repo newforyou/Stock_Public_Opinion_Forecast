df61db58081f7654d323180fbdc3820d4a9b2a31
yds <1451413307@qq.com>
2023-03-06 14:08:37 +0800

    更新前端页面url

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ def hello_world():  # put application's code here
 def index():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('details.html')
+
 
 if __name__ == '__main__':
     app.run()
