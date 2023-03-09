d7022fe83937d8f11b9d525797ca5b7018c7ad66
zkl <839416659@qq.com>
2023-03-09 15:44:13 +0800

    增加用户信息的前后端连接

--- blueprints/personal.py
+++ blueprints/personal.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash, jsonify
 from .forms import RegisterForm, LoginForm
 from models import UserModel,StockModel,SubscriptionModel
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -17,6 +17,26 @@ def index():
 @bp.route('/profile')
 def profile():
     return render_template('profile.html')
+@bp.route('/profile/<int:user_id>',methods=['GET','POST'])
+def update_profile(user_id):
+    userinfo = UserModel.query.get(user_id)
+    if request.method == 'GET':
+        return render_template('profile.html', user_info=userinfo)
+    else:
+        if userinfo:
+            # 读取表单数据
+            nickname = request.form.get('usernick')
+            age = request.form.get('userage')
+            mail = request.form.get('usermail')
+
+            userinfo.nickname = nickname
+            userinfo.age = age
+            userinfo.userMail = mail
+            db.session.commit()
+
+            return render_template('profile.html', user_info=userinfo)
+        else:
+            return jsonify({'status': 'error', 'message': '用户信息不存在'})
 
 
 @bp.route('/detail')
