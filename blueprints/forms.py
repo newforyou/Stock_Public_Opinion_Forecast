1699175034b797adf7a67068f636f086e355e497
yds <1451413307@qq.com>
2023-03-15 10:49:07 +0800

    forms更新注册

--- blueprints/forms.py
+++ blueprints/forms.py
@@ -11,18 +11,6 @@ class RegisterForm(wtforms.Form):
     password = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误！")])
     password_confirm = wtforms.StringField(validators=[EqualTo("password", message="两次密码不一致")])
 
-class UpdatePersonalForm(wtforms.Form):
-    nickname=wtforms.StringField(validators=[Length(min=1, max=14, message="昵称格式错误！")])
-    age = wtforms.StringField(validators=[Length(min=1, max=3, message="年龄格式错误！")])
-    userMail = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误！")])
-    signature=wtforms.StringField(validators=[Length(min=1, max=50, message="个性签名格式错误！")])
-
-class UpdatePasswordForm(wtforms.Form):
-    originpwd=wtforms.StringField(validators=[Length(min=1, max=14, message="不可用！")])
-    newpwd= wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误！")])
-    verifypwd=wtforms.StringField(validators=[EqualTo("newpwd", message="两次密码不一致")])
-
-
     # 自定义验证
     # 1.邮箱是否已经被注册
     def validate_email(self, field):
@@ -37,6 +25,20 @@ class UpdatePasswordForm(wtforms.Form):
         user = UserModel.query.filter_by(userName=username).first()
         if user:
             raise wtforms.ValidationError(message="用户名已经被注册！")
+
+
+class UpdatePersonalForm(wtforms.Form):
+    nickname = wtforms.StringField(validators=[Length(min=1, max=14, message="昵称格式错误！")])
+    age = wtforms.StringField(validators=[Length(min=1, max=3, message="年龄格式错误！")])
+    userMail = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误！")])
+    signature = wtforms.StringField(validators=[Length(min=1, max=50, message="个性签名格式错误！")])
+
+
+class UpdatePasswordForm(wtforms.Form):
+    originpwd = wtforms.StringField(validators=[Length(min=1, max=14, message="不可用！")])
+    newpwd = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误！")])
+    verifypwd = wtforms.StringField(validators=[EqualTo("newpwd", message="两次密码不一致")])
+
 
 class LoginForm(wtforms.Form):
     email = wtforms.StringField(validators=[Email(message="邮箱格式错误！")])
